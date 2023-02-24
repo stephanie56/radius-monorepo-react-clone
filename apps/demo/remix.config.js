939e77364a323703eafe0dd48e9977a0992e244f
Stephanie Zeng <stephanie56@users.noreply.github.com>
2023-02-24 08:39:51 -0500

    Update remix config

--- apps/demo/remix.config.js
+++ apps/demo/remix.config.js
@@ -2,7 +2,6 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: 'vercel',
 	// When running locally in development mode, we use the built in remix
 	// server. This does not understand the vercel lambda module format,
 	// so we default back to the standard build output.
@@ -10,7 +9,7 @@ module.exports = {
   ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  serverBuildPath: "api/index.js",
   serverDependenciesToBundle: ['@rangle/radius-react-core-foundations', '@rangle/radius-react-core-components'],
 };
